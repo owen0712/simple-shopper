ae80c6a4a33cfb68500bf41cb2778c277df17ae2
owen0712 <owentan712@gmail.com>
2021-04-03 14:57:26 +0800

    first commit

---
+++ js/address.js
@@ -0,0 +1,12 @@
+const addressForm=document.getElementById('add_address');
+const addAddressBtn=document.getElementById('add_btn');
+const addresses=
+function handleEdit(e){
+    const section=e.target
+}
+
+function showForm(){
+    addressForm.style.display='flex';
+}
+
+addAddressBtn.onclick=function(){showForm()};
