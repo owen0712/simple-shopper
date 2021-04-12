e8d502bda44ddd85452714a1d6be7c9df985c5c3
enghao10 <59091830+enghao10@users.noreply.github.com>
2021-04-12 18:12:48 +0800

    Add files via upload

---
+++ js/administrator.js
@@ -0,0 +1,7 @@
+document.getElementById("add_btn").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "flex";
+})
+
+document.getElementById("close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+})
