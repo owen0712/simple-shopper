c61eeef8a02c4fd570116c3bc598e836bb905334
cheekingneoh <cheeking8919@gmail.com>
2021-04-15 00:18:53 +0800

    Changed home page layout
    Changed the cards

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 AOS.init();
+
+const ele = document.getElementsByClassName('side-scroll');
+ele.scrollTop = 100;
+ele.scrollLeft = 150;
+
+let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+const mouseMoveHandler = function(e) {
+    // How far the mouse has been moved
+    const dx = e.clientX - pos.x;
+    const dy = e.clientY - pos.y;
+
+    // Scroll the element
+    ele.scrollTop = pos.top - dy;
+    ele.scrollLeft = pos.left - dx;
+};
