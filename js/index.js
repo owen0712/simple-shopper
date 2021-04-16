6cc9fe486744ccf4fa7a66363c512028d3d2ddb2
ZhaoWei00 <75360593+ZhaoWei00@users.noreply.github.com>
2021-04-17 03:03:56 +0800

    Revert "Merge branch 'main' of https://github.com/owen0712/simple-shopper into main"
    This reverts commit 918ea7edccb7e3a9efa9b5facf4d8e944bda8db5, reversing
    changes made to eb4ac9b5b224038f074647b72e1eb279f5317c12.

--- js/index.js
+++ js/index.js
@@ -15,31 +15,3 @@ const mouseMoveHandler = function(e) {
     ele.scrollTop = pos.top - dy;
     ele.scrollLeft = pos.left - dx;
 };
-
-document.getElementById("searchButton").addEventListener("click", function() {
-    var input, filter,shopctg, card, target, txtValue, ctgvalue;
-    input = document.getElementById("search");
-    filter = input.value.toUpperCase();
-    shopctg = document.getElementsByClassName("shopctg");
-    for(var i = 0;i<shopctg.length;i++)
-    {
-        card = shopctg[i].getElementsByClassName("card");
-        ctgvalue = 0;
-        for(var j = 0;j<card.length;j++)
-        {
-            target = card[j].getElementsByClassName("card-title")[0];
-            txtValue = target.textContent || target.innerText;
-            if(txtValue.toUpperCase().indexOf(filter)>-1)
-            {
-                card[j].style.display = "";
-            }else{
-                card[j].style.display = "none";
-                ctgvalue++;
-            }
-        }
-        if(ctgvalue == card.length)
-        {
-            shopctg[i].style.display = "none";
-        }else{shopctg[i].style.display = "";}
-    }
-});
