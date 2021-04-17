6d680e346a1d949b2ee3ae91befe0ff4bfbe0d57
cheekingneoh <cheeking8919@gmail.com>
2021-04-17 13:56:21 +0800

    Added footer and fixed item quantity
    Also moved all the asset images into the main asset folder

--- js/index.js
+++ js/index.js
@@ -43,3 +43,88 @@ document.getElementById("searchButton").addEventListener("click", function() {
         }else{shopctg[i].style.display = "";}
     }
 });
+
+
+
+function wcqib_refresh_quantity_increments() {
+    jQuery("div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)").each(function(a, b) {
+        var c = jQuery(b);
+        c.addClass("buttons_added"), c.children().first().before('<input type="button" value="-" class="minus" />'), c.children().last().after('<input type="button" value="+" class="plus" />')
+    })
+}
+String.prototype.getDecimals || (String.prototype.getDecimals = function() {
+    var a = this,
+        b = ("" + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+    return b ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0)) : 0
+}), jQuery(document).ready(function() {
+    wcqib_refresh_quantity_increments()
+}), jQuery(document).on("updated_wc_div", function() {
+    wcqib_refresh_quantity_increments()
+}), jQuery(document).on("click", ".plus, .minus", function() {
+    var a = jQuery(this).closest(".quantity").find(".qty"),
+        b = parseFloat(a.val()),
+        c = parseFloat(a.attr("max")),
+        d = parseFloat(a.attr("min")),
+        e = a.attr("step");
+    b && "" !== b && "NaN" !== b || (b = 0), "" !== c && "NaN" !== c || (c = ""), "" !== d && "NaN" !== d || (d = 0), "any" !== e && "" !== e && void 0 !== e && "NaN" !== parseFloat(e) || (e = 1), jQuery(this).is(".plus") ? c && b >= c ? a.val(c) : a.val((b + parseFloat(e)).toFixed(e.getDecimals())) : d && b <= d ? a.val(d) : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())), a.trigger("change")
+});
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() { myFunction() };
+
+// Get the header
+var header = document.getElementById("fixedScreen");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+$(document).ready(function() {
+    var mokData = [
+        { category: "Material", id: '1', name: 'Brakedown of machine' },
+        { category: "Material", id: '2', name: 'Brakedown of machine' },
+        { category: "Tool", id: '3', name: 'Brakedown of machine' },
+        { category: "Tool", id: '4', name: 'Brakedown of line' },
+        { category: "Tool", id: '5', name: 'Brakedown of machine' },
+        { category: "Tool", id: '6', name: 'Brakedown of line' },
+        { category: "Tool", id: '7', name: 'Brakedown of machine' },
+        { category: "Tool", id: '8', name: 'Brakedown of line' }
+    ];
+    /*<img src="Paper product/Paper Product-9.jpg" class="card-img-top" alt="Wax Paper" height="auto" width="auto">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Reynolds</h5>
+                                            <p class="card-text">Reynolds Wax Paper Sandwich Bag 50pcs</p>
+                                            <p style="font-size: small; float: right;"> RM 6.99/each</p>
+                                        </div>
+                                        <div class="card-body">
+                                            <div class="quantity buttons_added" style="float: left;">
+                                                <input type="button" value="-" class="minus"><input type="number" step="1" min="1" max="" name="quantity" value="1" title="Qty" class="input-text qty text" size="4" pattern="" inputmode=""><input type="button"
+                                                    value="+" class="plus">
+                                            </div>
+                                            <button type="button" class="btn btn-success" style="float: right;">Add to cart</button>
+                                        </div>*/
+
+    $.each(mokData, function(i) {
+
+        //var templateString = '<article class="card"><h2>' + mokData[i].category + '</h2><p>' + mokData[i].name + '</p><p>' + mokData[i].id + '</p><button id="tes">Start</button></article>';
+        //$('#test12').append(templateString);
+
+    })
+
+    for (var i = 0; i < 3; i++) {
+
+        for (j = 0; j < 4; j++) {
+            var templateString = '<div class="card"><img src="Paper product/Paper Product-9.jpg" class="card-img-top" alt="Wax Paper" height="auto" width="auto"> <div class="card-body"> <h5 class="card-title">Reynolds</h5><p class="card-text">Reynolds Wax Paper Sandwich Bag 50pcs</p><p style="font-size: small; float: right;"> RM 6.99/each</p></div><div class="card-body"><div class="quantity buttons_added" style="float: left;"><input type="button" value="-" class="minus"><input type="number" step="1" min="1" max="" name="quantity" value="1" title="Qty" class="input-text qty text" size="4" pattern="" inputmode=""><input type="button"value="+" class="plus"></div><button type="button" class="btn btn-success" style="float: right;">Add to cart</button></div></div>';
+            $('#test12').append(templateString + '&nbsp');
+        }
+
+    }
+
+    $("#test12");
+});
