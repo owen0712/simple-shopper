179cca5f70c75f413f5578d5783050c0cb774f66
owen0712 <owentan712@gmail.com>
2021-04-20 20:08:17 +0800

    add on check password feature

--- js/password.js
+++ js/password.js
@@ -3,22 +3,131 @@ const user={
     password:"simplekid123"
 }
 
-var new_password="";
+function showCurrentPassword(){
+    let x = document.querySelector("#current_password")
+    let y = document.querySelector("#hide1")
+    let z = document.querySelector("#hide2")
+    
+    if(x.type === 'password'){
+        x.type = "text";
+        y.style.display = "block";
+        z.style.display = "none";
+    }
+    else{
+        x.type = "password";
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+}
 
-$('#current_password').on('change',function(){//got bug
-    if($('current_password').val()!==user['password']){
-        alert('Wrong current password')
+function showNewPassword(){
+    let x = document.querySelector("#new_password")
+    let y = document.querySelector("#hide3")
+    let z = document.querySelector("#hide4")
+    
+    if(x.type === 'password'){
+        x.type = "text";
+        y.style.display = "block";
+        z.style.display = "none";
+    }
+    else{
+        x.type = "password";
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+}
+
+function showConfirmPassword(){
+    let x = document.querySelector("#confirm_password")
+    let y = document.querySelector("#hide5")
+    let z = document.querySelector("#hide6")
+    
+    if(x.type === 'password'){
+        x.type = "text";
+        y.style.display = "block";
+        z.style.display = "none";
+    }
+    else{
+        x.type = "password";
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+}
+
+const o_password = document.querySelector('#current_password')
+const n_password = document.querySelector('#new_password')
+const c_password = document.querySelector('#confirm_password')
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    if(checkCurrentPassword()&&checkNewPassword()&&checkConfirmPassword()){
+        user['password']=c_password.value.trim();
     }
 })
-$('#new_password').on('change',function(){
-    new_password=$('#new_password').val();
-});
-$('#confirm_password').on('change',function(){
-    if($(this).val()!==new_password){
-        alert("Wrong password");
-    }
-})
 
-$('.confirm').on('click',function(){
-    user['password']=$('current_password').val();
-})
+function checkCurrentPassword(){
+    const currentPasswordValue = o_password.value.trim();
+
+    if(currentPasswordValue === ''){
+        setErrorFor(o_password,'Password cannot be blank');
+        return false;
+    }else if(currentPasswordValue!== user['password']){
+        setErrorFor(o_password,'Different password');
+        return false;
+    }else{
+        setSuccessFor(o_password);
+        return true;
+    }
+}
+
+function checkNewPassword(){
+    const newPasswordValue = n_password.value.trim();
+
+    if(newPasswordValue === ''){
+        setErrorFor(n_password,'Password cannot be blank');
+        return false;
+    }else if(newPasswordValue.length < 8){
+        setErrorFor(n_password,'Password must more than 8 characters');
+        return false;
+    }else{
+        setSuccessFor(n_password);
+        return true;
+    }
+}
+
+function checkConfirmPassword(){
+    const newPasswordValue = n_password.value.trim();
+    const cPasswordValue = c_password.value.trim();
+
+    if(cPasswordValue === ''){
+        setErrorFor(c_password,'Password cannot be blank');
+        return false;
+    }else if(newPasswordValue!== cPasswordValue){
+        setErrorFor(c_password,'Different password');
+        return false;
+    }else{
+        setSuccessFor(c_password);
+        return true;
+    }
+}
+
+function setErrorFor(input, message){
+    const form_type = input.parentElement;
+    const small = form_type.querySelector('small');
+    small.innerText = message;
+    form_type.className = 'valid_section error';
+}
+
+function setSuccessFor(input){
+    const form_type = input.parentElement;
+    const small = form_type.querySelector('small');
+    small.innerText = '';
+    form_type.className = 'valid_section success';
+}
+
+function clear(){
+    const form_type = input.parentElement;
+    const small = form_type.querySelector('small');
+    small.innerText = '';
+    form_type.className = '';
+}
