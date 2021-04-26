5e865ec69e16ae4ea68b8d9421c9e62b4bcb3ada
owen0712 <owentan712@gmail.com>
2021-04-26 09:00:20 +0800

    update user file

--- js/user.js
+++ js/user.js
@@ -2,7 +2,6 @@
 const users=[{
     password:'simplekid123',
     status:'user',
-    login:false,
     name:"Sim Ple Kid",
     email:"simplekid@gmail.com",
     phone:"0123456789",
@@ -22,7 +21,6 @@ const users=[{
 {
     password:'admin123',
     status:'admin',
-    login:false,
     name:"AdminUser",
     email:"admin@gmail.com",
     phone:"0123456789",
@@ -40,13 +38,12 @@ const users=[{
     }]
 }]
 
-localStorage.setItem('users',JSON.stringify(users))
+//localStorage.setItem('users',JSON.stringify(users))
 
 //try
 var current_user={
     password:'admin123',
     status:'admin',
-    login:false,
     name:"AdminUser",
     email:"admin@gmail.com",
     phone:"0123456789",
@@ -64,42 +61,13 @@ var current_user={
     }]
 }
 
+function updateStorage(user){
+    var user_list=JSON.parse(localStorage.getItem('users'))
+    //for(var )
+}
+
 //localStorage.setItem('user',JSON.stringify(current_user))
 //until here please remove it after you run your program
-function signIn(input,type,password){
-    var check='';//ah boy code have to pass check phone or email
-    if(type==='email'){
-        check='email';
-    }
-    else if(type==='phone'){
-        check='phone';
-    }
-    users.forEach((user)=>{
-        if(user[check]===username&&user['password']===password){
-            user['login']=true;
-            localStorage.setItem('user',JSON.stringify(user));
-        }
-        return true;
-    })
-    return false;
-}
-
-function signUp(name,email,phone,password,dob,gender,status){
-    var new_user={
-        password:password,
-        status:status,//confirm with ah boy
-        login:false,
-        name:name,
-        email:email,
-        phone:phone,
-        gender:gender,
-        dob:dob,
-        addressesArr:[]
-    }
-    users.push(new_user)
-    localStorage.setItem('users',user)
-}
-
 function updatePassword(password){
     var user=JSON.parse(localStorage.getItem('user')||{});
     user['password']=password;
@@ -118,8 +86,8 @@ var user=JSON.parse(localStorage.getItem('user')||{})
 //admin section
 //set default invisible (haven't done   )
 const admin_anchor=document.querySelector('#admin')
-if(user['status']!=='admin'){
-    admin_anchor.innerHTML='';
+if(user['status']==='admin'){
+    admin_anchor.innerHTML='Administrator';
 }
 
 const signin_anchor=document.querySelector('#sign-in')
