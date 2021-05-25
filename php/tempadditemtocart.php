7387ebcd87cc186f4c954b3512fffd07bd838b81
siayiheng0219 <81074268+siayiheng0219@users.noreply.github.com>
2021-05-25 19:50:45 +0800

    Update tempadditemtocart.php

--- php/tempadditemtocart.php
+++ php/tempadditemtocart.php
@@ -1,8 +1,9 @@
 <?php
+    session_start();
     if(isset($_POST['quantity']))
     {
         $quantity = $_POST['quantity'];
-        $id = $_GET['id'];
+        $id = $_SESSION["id"];
         echo $quantity."<br>";
         echo $id;
         // header('Location:index.php');
